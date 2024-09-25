ec1a85cd059bed78c82827ea38cd8e4ef2ce9166
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-09-25 14:46:30 +0200

    Uncrispyfy JustificationForm in arnold

--- python/nav/web/arnold/views.py
+++ python/nav/web/arnold/views.py
@@ -155,8 +155,10 @@ def render_justifications(request, jid=None):
                 'description': justification.description,
             }
         )
+        form.attrs.method = 'post'
     else:
         form = JustificationForm()
+        form.attrs.method = 'post'
 
     justifications = Justification.objects.all()
     for justification in justifications:
